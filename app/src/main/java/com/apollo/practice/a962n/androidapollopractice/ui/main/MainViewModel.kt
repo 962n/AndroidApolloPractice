e8f8bcc798e51ff190f79e805ce7121324b141cd
962n <tak.k1u2r2o4@gmail.com>
2019-01-18 22:50:12 +0900

    Add New Project

---
+++ app/src/main/java/com/apollo/practice/a962n/androidapollopractice/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.apollo.practice.a962n.androidapollopractice.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
